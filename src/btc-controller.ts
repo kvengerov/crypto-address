868b82fae622914341081cb50b098acb79c3a1af
konstantin.vengerov@agilie.com <Q31081986q>
2019-07-04 12:14:55 +0300

    Refactored controllers

--- src/btc-controller.ts
+++ src/btc-controller.ts
@@ -2,15 +2,15 @@ var bitcore = require('bitcore-lib');
 
 export function getBtc() {
   var string = document.querySelector<HTMLInputElement>('input#btc').value;
-  console.log(string);
+  document.getElementById('btcAddress').innerHTML = createAdrress(string);
+}
 
+function createAdrress(string) {
   // https://github.com/bitpay/bitcore-lib/blob/HEAD/docs/examples.md#generate-a-address-from-a-sha256-hash
   // https://github.com/bitpay/bitcore-lib/blob/master/docs/crypto.md
-
   var value = Buffer.from(string);
   var hash = bitcore.crypto.Hash.sha256(value);
   var bn = bitcore.crypto.BN.fromBuffer(hash);
   var address = new bitcore.PrivateKey(bn).toAddress();
-
-  document.getElementById('btcAddress').innerHTML = address;
+  return address;
 }
--- src/eth-controller.ts
+++ src/eth-controller.ts
@@ -2,8 +2,10 @@ var ethers = require('ethers');
 
 export function getEth() {
   var string = document.querySelector<HTMLInputElement>('input#eth').value;
-  console.log('Введенная строка:', string);
- 
+  document.getElementById('ethAddress').innerHTML = createAdrress(string);
+}
+
+function createAdrress(string) {
   // https://docs.ethers.io/ethers.js/html/api-utils.html#bytes32-strings
   var messageBytes = ethers.utils.toUtf8Bytes(string);
   var privateKey = ethers.utils.sha256(messageBytes);
@@ -11,6 +13,5 @@ export function getEth() {
   // https://docs.ethers.io/ethers.js/html/api-wallet.html
   var wallet = new ethers.Wallet(privateKey);
   console.log('wallet.address', wallet.address);
-
-  document.getElementById('ethAddress').innerHTML = wallet.address;
+  return wallet.address;
 }
